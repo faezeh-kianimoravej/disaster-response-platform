685c0751bf4e6d936b9ecb4c35861d887fe6b82a
Ben Vos <ben.j.vos@gmail.com>
2025-11-15 17:25:41 +0100

    ✨ feat(kafka): remove JsonSerializer ADD_TYPE_INFO_HEADERS config

--- backend-app/deployment-service/src/main/java/nl/saxion/disaster/deploymentservice/configuration/KafkaProducerConfig.java
+++ backend-app/deployment-service/src/main/java/nl/saxion/disaster/deploymentservice/configuration/KafkaProducerConfig.java
@@ -23,9 +23,6 @@ public class KafkaProducerConfig {
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
-        // Optional but recommended
-        config.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-
         return new DefaultKafkaProducerFactory<>(config);
     }
 
