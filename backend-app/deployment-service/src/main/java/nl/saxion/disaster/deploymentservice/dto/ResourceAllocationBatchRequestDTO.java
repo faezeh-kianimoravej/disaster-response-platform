29a9c6bf1cacd4d055c46cd3c6e08a73b824d0b2
fkiani <586010@student.saxion.nl>
2025-11-30 15:10:54 +0100

    Bug fix- change the dto to sending info to resource service in deployment process

--- backend-app/deployment-service/src/main/java/nl/saxion/disaster/deploymentservice/dto/ResourceAllocationBatchRequestDTO.java
+++ backend-app/deployment-service/src/main/java/nl/saxion/disaster/deploymentservice/dto/ResourceAllocationBatchRequestDTO.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public record ResourceAllocationBatchRequestDTO(
         Long deploymentId,
-        Long unitId,
-        List<ResourceAllocationItemDTO> items
+        Long responseUnitId,
+        List<ResourceAllocationItemDTO> allocations
 ) {}
